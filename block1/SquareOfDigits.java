2e4f2e3bce3afaa0995128e4c2b980069e663817
Sk Md Shariful Islam Arafat <a1983627@ingb16w029.ad.adelaide.edu.au>
2025-08-05 14:38:45 +0930

    init of 2nd problem

---
+++ block1/SquareOfDigits.java
@@ -0,0 +1,11 @@
+public class SquareOfDigits {
+    
+    public static int getMax(String[] data) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMax(new String[]{"42101", "22100", "22101"}));
+    }
+}
