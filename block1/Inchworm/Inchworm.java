7e05d065698d89a7e8f9e3ece4ab6e9d511824f0
Sk Md Shariful Islam Arafat <a1983627@ingb16w029.ad.adelaide.edu.au>
2025-08-05 14:03:58 +0930

    removed main method

--- block1/Inchworm/Inchworm.java
+++ block1/Inchworm/Inchworm.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
 
 public class Inchworm {
-    public static int lunchtime(int branch, int rest, int leaf) {
+    public int lunchtime(int branch, int rest, int leaf) {
         ArrayList<Integer> leaves = new ArrayList<>();
         for(int i=0;i<=branch;i+=leaf) {
             leaves.add(i);
         }
-        System.out.println(leaves);
         int count = 0;
         for (int i: leaves) {
             if (i % rest == 0)
@@ -14,9 +13,5 @@ public class Inchworm {
         }
 
         return count;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(lunchtime(1000, 7, 3));
     }
 }
