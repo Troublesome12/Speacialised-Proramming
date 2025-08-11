13190a722bc594dd77921234703e61875d01b85d
Shariful Islam <arafat@Sharifuls-MacBook-Air.local>
2025-08-11 18:35:28 +0930

    Week 1 practice problem added

--- block1/AlternateColors/AlternateColors.java
+++ block1/AlternateColors/AlternateColors.java
@@ -1,27 +1,42 @@
 public class AlternateColors {
-    
     public String getColor(long r, long g, long b, long k) {
-        String[] temp = new String[(int) k + 3];
+        long fullCycles = Math.min(r, Math.min(g, b));
+        long fullCycleBalls = fullCycles * 3;
 
-        for (int i=0; i<=k;) {
-            for (int j=1; j<=3; j++) {
-                if ((j % 3 == 1) && (r > 0)) {
-                    temp[i] = "RED";
-                    r--;
-                    i++;
-                }
-                else if ((j % 3 == 2) && (g > 0)) {
-                    temp[i] = "GREEN";
-                    g--;
-                    i++;
-                }
-                else if ((j % 3 == 0) && (b > 0)) {
-                    temp[i] = "BLUE";
-                    b--;
-                    i++;
-                }
+        if (k <= fullCycleBalls) {
+            int pos = (int)(k % 3);
+            if (pos == 1) return "RED";
+            if (pos == 2) return "GREEN";
+            return "BLUE"; // pos == 0
+        }
+
+        long leftK = k - fullCycleBalls;
+
+        long[] counts = new long[] {r - fullCycles, g - fullCycles, b - fullCycles};
+        String[] colors = new String[] {"RED", "GREEN", "BLUE"};
+
+        // Filter colors with leftover > 0 and remember their indexes
+        int availableCount = 0;
+        int[] availableIndices = new int[3];
+        for (int i = 0; i < 3; i++) {
+            if (counts[i] > 0) {
+                availableIndices[availableCount++] = i;
             }
         }
-        return temp[(int) k - 1];
+
+        int cycleLen = availableCount;
+        long fullLeftCycles = (leftK - 1) / cycleLen;
+        int posInCycle = (int)((leftK - 1) % cycleLen);
+
+        // Find the color index in the leftover cycle that still has balls left
+        for (int i = 0; i < cycleLen; i++) {
+            int idx = availableIndices[(posInCycle + i) % cycleLen];
+            if (counts[idx] > fullLeftCycles) {
+                return colors[idx];
+            }
+        }
+
+        // Should never happen
+        return "UNKNOWN";
     }
 }
