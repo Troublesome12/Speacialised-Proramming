3df89ffd48a1393249e80e381c876046b7fe514b
Shariful Islam <arafat@Sharifuls-MacBook-Air.local>
2025-08-14 13:00:21 +0930

    RugSize problem solved

--- block1/RugSizes/RugSizes.java
+++ block1/RugSizes/RugSizes.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+
 public class RugSizes {
-    
-    public	int rugCount(int area) {
+
+    public int rugCount(int area) {
         int count = 0;
         int j;
+        ArrayList<Integer> multiplier = new ArrayList<Integer>();
+
         for(int i=1; i<area; i++) {
             if (area%i == 0) {
                 j = area/i;
 
                 if ((i%2 != 0 || j%2 != 0) || (i == j)){
+                    if (multiplier.contains(i)) {
+                        continue;
+                    }
+                    multiplier.add(j);
                     count++;
                 }
             }
